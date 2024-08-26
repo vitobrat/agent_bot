724ced60618858bb3f87a18f838a1c8107d73af3
Victor Bratko <bratko.viktor2004@gmail.com>
2024-08-26 18:17:51 +0300

    left dafault prompts, because longs one call unstable behavior of model

--- src/agent/prompts.py
+++ src/agent/prompts.py
@@ -3,24 +3,7 @@ This is store for all prompts from agent.main.py
 """
 from langchain_core.prompts import PromptTemplate
 
-SYSTEM_AGENT_PROMPT = ("You are an AI assistant specifically designed to assist with queries related to cryptocurrency."
-                       " Your primary task is to retrieve information from the vector store,"
-                       " which contains documents specifically related to cryptocurrency topics."
-                       " If the user's query is related to cryptocurrency,"
-                       " search the vector store for relevant information and provide a detailed response."
-                       " If the user's query is not related to cryptocurrency,"
-                       " kindly inform them that you can only assist with cryptocurrency-related questions."
-                       " Always respond in a polite and professional manner."
-                       " Your responses must not contain harmful, unethical, racist, sexist, toxic,"
-                       " dangerous or illegal content."
-                       " Please ensure that your responses are socially unbiased and positive in nature."
-                       "Remember, you should only use the information stored"
-                       " in the vector store to answer questions related to cryptocurrency."
-                       "Don't ask the user for a time interval, "
-                       "answer the query using all the information provided from the vector store."
-                       "Don't say you don't know the information in real time, "
-                       "provide all the information from the repository on the subject of the query."
-                       "If you don't know the answer to a question, please don't spread false information.")
+SYSTEM_AGENT_PROMPT = "You are an AI assistant specifically designed to assist with queries related to cryptocurrency."
 
 SYSTEM_SUM_PROMPT = ("Твоя задача написать краткое содержание новости по теме криптовалют."
                      "Напиши главный смысл новости используя 1 или 2 предложения.")
@@ -34,7 +17,8 @@ SYSTEM_TRANSLATE_ENG_PROMPT = ("Your task is to translate this text into English
                                "If text already in english, then just return origin text."
                                "Your response have to consist only translated text")
 
-USER_PROMPT = PromptTemplate.from_template("This is my query: {query}")
+USER_PROMPT = PromptTemplate.from_template("Answer only on cryptocurrency relate query."
+                                           "This is my query: {query}")
 
 QUERY_PROMPT = """You are an assistant tasked with taking a natural language query from a user
             and converting it into a query for a vectorstore. In the process, strip out all 
